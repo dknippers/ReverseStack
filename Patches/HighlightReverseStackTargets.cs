4ee2c1e14987791fef1dd64684879def6c5d8629
Daniël Knippers <dknippers@gmail.com>
2023-12-25 00:20:10 +0100

    Highlight cards that allow Reverse Stack with the card being dragged

---
+++ Patches/HighlightReverseStackTargets.cs
@@ -0,0 +1,33 @@
+using HarmonyLib;
+using SmartStack.Extensions;
+using UnityEngine;
+
+namespace SmartStack.Patches;
+
+/// <summary>
+/// Highlight targets for a Reverse Stack operation.
+/// For an explanation of a Reverse Stack see <see cref="AttemptReverseStackAfterFailedStack"/>.
+/// </summary>
+[HarmonyPatch]
+public static class HighlightReverseStackTargets
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(GameCard), "Update")]
+    public static void GameCard_Update(GameCard __instance)
+    {
+        if (__instance is not GameCard card ||
+            card.HighlightRectangle.enabled ||
+            WorldManager.instance.DraggingCard is not GameCard draggingCard ||
+            !card.IsRoot() ||
+            card.IsSameStack(draggingCard) ||
+            card.GetLeafCard().HighlightActive ||
+            !draggingCard.CanHaveOnTop(card))
+        {
+            return;
+        }
+
+        // Reverse Stack highlight
+        card.HighlightRectangle.enabled = true;
+        card.HighlightRectangle.Color = Color.black;
+    }
+}
