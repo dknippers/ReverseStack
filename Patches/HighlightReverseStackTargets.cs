8d7e9013d527c3ba761b79189fc231d92f10b036
Daniël Knippers <dknippers@gmail.com>
2023-12-27 23:59:41 +0100

    Update highlight style to look like a drop shadow
    The dashed border color actually changes between mainland and island
    with island using black by default, which we thought was an unused color
    so we used it before. On the island you could no longer tell which
    highlight was for default stack and which was for reverse stack.
    
    We experimented with the inverse color but white on the island is
    almost invisible due to the lighter background. Instead we now opt
    for a solid border and make it gray ish in every world so it looks like
    a drop shadow

--- Patches/HighlightReverseStackTargets.cs
+++ Patches/HighlightReverseStackTargets.cs
@@ -11,22 +11,54 @@ namespace ReverseStack.Patches;
 [HarmonyPatch]
 public static class HighlightReverseStackTargets
 {
+    private const bool DEFAULT_DASHED = true;
+    private const float DEFAULT_CORNER_RADIUS = 0f;
+    private const float DEFAULT_THICKNESS = 0.022f;
+    private const float DEFAULT_HEIGHT = 0.5171013f;
+    private const float DEFAULT_WIDTH = 0.435f;
+
+    private const bool RS_DASHED = false;
+    private const float RS_CORNER_RADIUS = 0.02f;
+    private const float RS_THICKNESS = 0.032f;
+    private const float RS_ALPHA = 0.8f;
+
+    private const float RS_PADDING = -0.01f;
+    private const float RS_DELTA = RS_THICKNESS - DEFAULT_THICKNESS;
+    private const float RS_HEIGHT = DEFAULT_HEIGHT + RS_DELTA + RS_PADDING;
+    private const float RS_WIDTH = DEFAULT_WIDTH + RS_DELTA + RS_PADDING;
+
     [HarmonyPostfix]
     [HarmonyPatch(typeof(GameCard), "Update")]
     public static void GameCard_Update(GameCard __instance)
     {
-        if (__instance is not GameCard card ||
-            card.HighlightRectangle.enabled ||
-            WorldManager.instance.DraggingCard is not GameCard draggingCard ||
+        if (__instance is not GameCard card || card.HighlightActive) return;
+
+        if (WorldManager.instance.DraggingCard is not GameCard draggingCard ||
             !card.IsRoot() ||
             card.CanHaveOnTop(draggingCard) ||
             !draggingCard.CanReverseStackOn(card))
         {
+            if (card.HighlightRectangle.Dashed != DEFAULT_DASHED)
+            {
+                // Revert our custom highlighting style if it is still applied
+                card.HighlightRectangle.Dashed = DEFAULT_DASHED;
+                card.HighlightRectangle.CornerRadius = DEFAULT_CORNER_RADIUS;
+                card.HighlightRectangle.Thickness = DEFAULT_THICKNESS;
+                card.HighlightRectangle.Height = DEFAULT_HEIGHT;
+                card.HighlightRectangle.Width = DEFAULT_WIDTH;
+            }
+
             return;
         }
 
         // Reverse Stack highlight
+        card.HighlightActive = true;
         card.HighlightRectangle.enabled = true;
-        card.HighlightRectangle.Color = Color.black;
+        card.HighlightRectangle.Dashed = RS_DASHED;
+        card.HighlightRectangle.Color = new Color(0, 0, 0, RS_ALPHA);
+        card.HighlightRectangle.CornerRadius = RS_CORNER_RADIUS;
+        card.HighlightRectangle.Thickness = RS_THICKNESS;
+        card.HighlightRectangle.Height = RS_HEIGHT;
+        card.HighlightRectangle.Width = RS_WIDTH;
     }
 }
